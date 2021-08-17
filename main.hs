a13713afc329577aec49241bd5a0dfd86e2b2b84
lucas-galdino <lucas.galdino.silva@ccc.ufcg.edu.br>
2021-08-16 22:32:58 -0300

    fazendo login de adm

--- main.hs
+++ main.hs
@@ -1,4 +1,6 @@
 import System.Exit (exitSuccess)
+import System.Directory
+import System.IO
 
 main :: IO()
 main = do
@@ -52,6 +54,15 @@ menuAdministrador = do
 	opcao <- getLine
 	opcaoAdministrador opcao
 
+segundoMenuAdministrador :: IO()
+segundoMenuAdministrador = do
+	putStrLn("Escolha o que você deseja fazer:\n")
+	putStrLn("1 - Cadastrar uma nova Perguntinha")
+	putStrLn("2 - Modificar ranking")
+	putStrLn("3 - Retornar para o menu")
+	opcao <- getLine
+	segundaTelaOpcaoAdministrador opcao
+
 opcaoAdministrador :: String -> IO()
 opcaoAdministrador x
 	| x == "1" = loginAdm
@@ -59,10 +70,58 @@ opcaoAdministrador x
 	| x == "3" = showMenu
 	| otherwise = invalidOption menuAdministrador
 
+segundaTelaOpcaoAdministrador :: String -> IO()
+segundaTelaOpcaoAdministrador x
+	| x == "1" = print "cadastraPergunta" -- test
+	| x == "2" = print "modificaRanking" -- test
+	| x == "3" = showMenu
+	| otherwise = invalidOption menuAdministrador
+
 showRecordes :: IO()
 showRecordes = do
 	putStrLn("Abaixo estão os nicknames dos jogadores que conseguiram fazer atingiram o maior ápice durante uma partida de Perguntinhas")
-	recordes -- mostra os recordes. aqui vai precisar de uma opcao para retornar ao menu dentro dessa funcao. algo do tipo "pressione qualquer botao para retornar ao menu".
+	--recordes -- mostra os recordes. aqui vai precisar de uma opcao para retornar ao menu dentro dessa funcao. algo do tipo "pressione qualquer botao para retornar ao menu".
+
+-- Como só existe um adm, não achei necessário ter nome de usuário, apenas senha e tbm simplifica se for assim.
+-- Não sei se eh a melhor forma de fazer, mas pelo que testei, funciona kkk.
+-- Acho que pode ter uma opção de resetar ou modificar a senha. 
+loginAdm :: IO()
+loginAdm = do
+    adminCadastrado <- doesFileExist "admin.txt"
+    
+    if adminCadastrado then do
+        putStr "Insira sua senha: "
+        senha <- getLine
+        file <- openFile "admin.txt" ReadMode
+        senhaCadastrada <- hGetContents file
+        
+        if senha == senhaCadastrada then do
+			putStrLn "Login realizado com sucesso."
+			segundoMenuAdministrador
+        else do
+			putStrLn "Senha incorreta, tente novamente.\n"
+			menuAdministrador
+        hClose file
+    
+    else do
+        putStrLn "Senha não cadastrada. Por favor, cadastre uma senha."
+        criaAdm
+
+
+criaAdm :: IO ()
+criaAdm = do
+    adminCadastrado <- doesFileExist "admin.txt"
+    if not adminCadastrado then do
+        putStrLn "Insira sua senha: "
+        senha <- getLine
+        file <- openFile "admin.txt" WriteMode
+        hPutStr file senha
+        putStrLn "Administrador criado com sucesso."
+        hFlush file
+        hClose file
+    else do
+        putStrLn "Administrador já cadastrado, utilize sua senha para logar."
+        loginAdm
 
 sobre :: IO()
 sobre = do

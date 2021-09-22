afeb314bb07e654a95c1bf1cfc8a333da46e1990
unknown <brenda.alves@ccc.ufcg.edu.br>
2021-09-22 16:46:53 -0300

    Correção no read.

--- Prolog/administrador.pl
+++ Prolog/administrador.pl
@@ -12,21 +12,20 @@ cadastra_adm(S) :-
 
 cadastraAdm :- 
 	nl, write("Insira sua senha: "), nl,
-	read(Senha),
+	read_line_to_String(Senha),
 	cadastra_adm(Senha).
 
 excluiAdm :- 
 	nl, write("Digite sua senha para confirmar exclusão: "), nl,
-	read(Senha),
+	read_line_to_String(Senha),
 	setup_bd,
-	(administrador(Senha)) -> exclui_cat(Senha), writeln("Administrador excluído com sucesso!");
+	(administrador(Senha)) -> exclui_adm(Senha), writeln("Administrador excluído com sucesso!");
 	write("Senha incorreta. Tente novamente."), nl.
 
-exclui_cat(Senha) :- 
+exclui_adm(Senha) :- 
 	setup_bd,
 	retract(administrador(Senha)),
 	tell('./data/bd_adm.pl'), nl,
-		listing(administrador/2),
-		told.
+	told.
 
 

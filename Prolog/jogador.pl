12f08565a44216057a64cf26fa779a9a7a4f4383
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2021-09-29 16:24:42 -0300

    metodo de responder questao ainda incompleto

--- Prolog/jogador.pl
+++ Prolog/jogador.pl
@@ -12,27 +12,44 @@ iniciaJogo :-
 	(X <= 15) -> inicia_jogo, nl;
 	write("Seu nome é maior que 15 caracteres, tente novamente."), iniciaJogo, nl.
 
-inicia_jogo (Nome, pontuacoes, questoesSorteadas, R) :- 
-	last(pontuacoes, R),
-	(R < 0) -> writeln("Sua pontuação foi menor que zero. Sua partida será encerrada, mas seu nome e seu ápice serão guardados no ranking."),
-	cadastra_no_ranking(Nome, (getApex(pontuacoes, valorMaximo)),
-	mostraMenu;
+inicia_jogo(Nome, UltimaPontuacao, Apex, QuestoesSorteadas) :-
+	UltimaPontuacao < 0,
+	halt.
+inicia_jogo(Nome, UltimaPontuacao, Apex, QuestoesSorteadas) :-
+	%totalQuestoes = numero total de questoes cadastradas no sistema
+	totalQuestoes =:= 0,
+	writeln("Ainda não há questões cadastradas no sistema."),
+	main,
+	halt.
+inicia_jogo(Nome, UltimaPontuacao, Apex, QuestoesSorteadas) :-
+	length(QuestoesSorteadas, Len),
+	%totalQuestoes = numero total de questoes cadastradas no sistema
+	Len =:= totalQuestoes,
+	writeln("Todas as perguntas já foram respondidas. Seu nome e ápice serão guardados no ranking e o jogo será encerrado."),
+	cadastra_no_ranking(Nome, UltimaPontuacao),
+	main,
+	halt.
+inicia_jogo(Nome, UltimaPontuacao, Apex, QuestoesSorteadas) :-
+	%totalQuestoes = numero total de questoes cadastradas no sistema (usar o predicado line_count)
+	random_between(0, totalQuestoes, Rand),
+	getQuestao(Rand, Q),
+	exibeQuestao(Q),
+	get_time(exibiu),
+	append(QuestoesSorteadas, [Rand], QuestoesSorteadas1),
+	read(Alternativa),
+	get_time(respondeu),
+	diff = timediff(exibiu, respondeu, dif),
+	respondeQuestao(Q, Alternativa, Pontuacao, DiffTempo),
+	(Pontuacao >= Apex -> inicia_jogo(Nome, Pontuacao, Pontuacao, QuestoesSorteadas1));
+	inicia_jogo(Nome, Pontuacao, Apex, QuestoesSorteadas1).
 
 
-getApex(L, R) :- 
-    length(L, Len),
-    Len =:= 1,
-    [H|T] = L,
-    R = H.
+timediff(DT1, DT2, Secs) :-
+        date_time_stamp(DT1, TS1),
+        date_time_stamp(DT2, TS2),
+        Sec is TS2 - TS1.
 
-getApex(L, R) :-
-    [H|T] = L,
-    getApex(T, AT),
-    H >= AT,
-    R = H.
-
-getApex(L, R) :-
-    [H|T] = L,
-    getApex(T, AT),
-    H < AT,
-    R = AT.
+respondeQuestao(Q, Alternativa, Pontuacao, DiffTempo) :-
+	gabarito(Q, G),
+	Alternativa =:= G,
+	
